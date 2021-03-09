976440ed43db0027304db461555802ab1b7adb6a
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-03-09 01:25:53 -0800

    fixed form colors

---
+++ includes/header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+
+
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+    <style>
+        .bg-dark{
+            background-color:#1a1c23!important
+        }
+        .bg-gradient-primary{
+            background-color: #121317!important;
+            background-image:linear-gradient(180deg,#121317 10%,#121317 100%);
+            background-size:cover;
+        }
+        .dark-border{
+            box-sizing:border-box;
+            border:0 solid #d5d6d7;
+        }
+        .form-control{
+            border: #4c4f52;
+            background-color: #24262d;
+        }
+        .btn{
+            border-radius: 10px;
+        }
+
+        .btn-primary{
+            background-color:#007bff!important
+        }
+
+
+        #wrapper #content-wrapper #content {
+            flex: 1 0 auto;
+            background-color: #121317!important;
+        }
+
+        .sidebar-bg{
+            background-color: #18191d;
+        }
+
+        .card{
+            background-color: #1a1c23!important;
+            border:0;
+        }
+
+        
+        .card-header{
+            background-color: #1a1c23!important;
+            border:0;
+        }
+
+        .uil{
+            font-size: 18px!important;
+        }
+
+        .sidebar-heading{
+            margin-bottom: 5px;
+            margin-top: 5px;
+        }
+
+        .nav-item{
+            margin-bottom: 4px;
+        }
+
+    </style>
+
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
